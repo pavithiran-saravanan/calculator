b4abe58f5db7211e5b31d0b58dc9d1de01ddd8a7
pavithiran <pavithransaravanan22@gmail.com>
2023-05-06 09:42:27 +0530

    Add decimal support

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const display = document.querySelector('.display');
+
 // Add
 function add(num1, num2){
     return num1+num2;
@@ -15,6 +17,7 @@ function multiply(num1, num2){
 
 // Divide
 function divide(num1, num2){
+    if(num2 == 0) return "LMAO"
     return num1/num2;
 }
 
@@ -33,10 +36,11 @@ function operate(num1, operator, num2){
 }
 
 // Create a function to populate the display when number buttons are clicked
-const display = document.querySelector('.display');
 function populateDisplay(){
     if(!num1) display.textContent = "0";
     else display.textContent = displayValue;
+
+    /* TO DO: ROUND LONG DECIMALS */
 };
 
 // Adding evenlistener to all of the number buttons. On click event listener to populate display.
@@ -44,6 +48,7 @@ const numbers = document.querySelectorAll('.num');
 numbers.forEach((number)=>{
     number.addEventListener('click', (e)=>{
         // Until operator is pressed, keep on adding selected numbers to num1
+        if(num1 == "LMAO") num1 = "";
         if(!operator){
             if(+num1 || e.target.getAttribute('id') != '0') num1 = num1 + e.target.getAttribute('id');
         }
@@ -62,8 +67,6 @@ numbers.forEach((number)=>{
 // Adding evetlistener to clear button to reset to default values
 const clear = document.querySelector('.clear');
 clear.addEventListener('click', (e)=>{
-    console.log('clear clicked');
-
     // Reset our variables
     num1 = num2 = operator = "";
 
@@ -81,12 +84,12 @@ function displayOperator(op){
 const operators = document.querySelectorAll('.op');
 operators.forEach((op)=>{
     op.addEventListener('click', (e)=>{
+        if(isDecimalPresentAtLast()) return;
         if(num1){
             // When operator is pressed after num1 is populated
             if(!num2){
                 operator = e.target.getAttribute('id');
                 displayValue = num1+displayOperator(operator)+num2;
-                console.log(num1, operator, num2);
                 populateDisplay();
             }
             // When operator is pressed after num2
@@ -101,14 +104,39 @@ operators.forEach((op)=>{
     })
 }) 
 
+function isDecimalPresentAtLast(){
+    if(num1 && num1[num1.length-1] == '.') return true;
+    if(num2 && num2[num2.length-1] == '.') return true;
+    else return false;
+}
+
 // Equal To Operation
 const equal = document.querySelector('#equal');
 equal.addEventListener('click', (e)=>{
-    if(num1 && operator && num2){
+    if(!isDecimalPresentAtLast() && num1 && operator && num2){
         num1 = operate(num1, operator, num2);
         operator = "";
         num2 = "";
         displayValue = num1;
         populateDisplay();
     }
+})
+
+const dot = document.querySelector('.dot');
+dot.addEventListener('click', (e)=>{
+    // If num1 == "" dot should be appended to a zero making 0.1
+    if(!num1){
+        num1 = "0.";
+    }
+    else if(!operator){
+        if(!num1.includes(".")) num1 = num1 + ".";
+    }
+    else if(!num2){
+        num2 = "0.";
+    }
+    else{
+        if(!num2.includes(".")) num2 = num2 + ".";
+    }
+    displayValue = num1 + operator + num2;
+    populateDisplay();
 })
